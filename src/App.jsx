a50930505eed9f95af27ede709666dcc29f763eb
Shahin992 <shahinkhan5979@gmail.com>
2023-09-15 11:43:34 +0600

    commit with remaining,hour and total calculation

--- src/App.jsx
+++ src/App.jsx
@@ -5,11 +5,38 @@ import Cards from './Component/Cards/Cards'
 import Cart from './Component/Cart/Cart'
 
 function App() {
+  let Remaining = 20
   const [selected,setSelected] = useState([])
+  const [total,setTotal] = useState(0)
+  const [hour,setHour] = useState(0)
+  let [remaining,setRemaining] = useState(Remaining);
+  
+  
+  
+
 
   const selectedBtn =cardDetails =>{
     const newSelected = [...selected,cardDetails]
-    setSelected(newSelected);
+   
+
+    let creditHour = cardDetails.credit_hour;
+    let count = cardDetails.price ;
+    
+    selected.forEach((item)=>{
+      count = count +item.price
+      creditHour = creditHour + item.credit_hour
+    
+    }
+      
+      ) 
+
+      remaining = Remaining - creditHour;
+
+      setSelected(newSelected);
+      setHour(creditHour);
+      setTotal(count);
+      setRemaining(remaining);
+      
 
   }
   
@@ -21,7 +48,7 @@ function App() {
       <h1 className='text-7xl font-bold mb-10'>Course Registration</h1>
       <div className='flex gap-8'>
         <Cards selectedBtn={selectedBtn}></Cards>
-        <Cart selected={(selected)}></Cart>
+        <Cart remaining={remaining} hour={hour} total={total} selected={(selected)}></Cart>
 
       </div>
       
--- src/Component/Cart/Cart.jsx
+++ src/Component/Cart/Cart.jsx
@@ -1,11 +1,11 @@
 import React from 'react';
 
-const Cart = ({selected}) => {
+const Cart = ({selected,hour,total,remaining}) => {
     
     return (
         <div className='w-1/3 text-left'>
            <div className='border-b-4'>
-           <h4 className='text-lg font-bold pb-4 '>Credit Hour Remaining: </h4>
+           <h4 className='text-lg font-bold pb-4 '>Credit Hour Remaining: {remaining} hr </h4>
            </div>
            <h4 className='text-lg font-semibold my-4'>Course Name: </h4>
 
@@ -25,7 +25,8 @@ const Cart = ({selected}) => {
             )
         } 
 
-<h4 className='text-lg font-semibold my-4 border-y-4 pt-5 pb-2'>Total Credit Hour : </h4>
+<h4 className='text-lg font-semibold my-4 border-y-4 pt-5 pb-2'>Total Credit Hour :  {hour} hr</h4>
+<h4 className='text-lg font-semibold my-4 border-y-4 pt-5 pb-2'>Total Price :  {total} USD</h4>
 
 
 
