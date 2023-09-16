2f67429bafb70b2650e9b113a4776fd33a0f8dc6
Shahin992 <shahinkhan5979@gmail.com>
2023-09-16 11:51:23 +0600

    icon change

--- src/Component/Cards/Card.jsx
+++ src/Component/Cards/Card.jsx
@@ -1,4 +1,6 @@
- import { FaBookOpen, FaDollarSign} from 'react-icons/fa';
+//  import { FaBookOpen, FaDollarSign} from 'react-icons/fa';
+ import {FiDollarSign} from 'react-icons/fi';
+  import {BsBook} from 'react-icons/bs';
 
 const Card = ({card,selectedBtn}) => {
     const {image,title,details,price,credit_hour} = card;
@@ -8,10 +10,14 @@ const Card = ({card,selectedBtn}) => {
             <h4 className="text-left text-lg h-10 font-semibold mb-2">{title}</h4>
             <p className='text-left mb-2 h-20'>{details}</p>
             <div className="mt-4 flex justify-between items-center">
-                <span className='text-xl'><FaDollarSign></FaDollarSign></span>
-                <p className="font-medium text-xl ">Price: {price}</p> 
-                <span className='text-xl'><FaBookOpen></FaBookOpen></span>
+                <div className='flex justify-center items-center gap-1'>
+                    <span className='text-3xl'><FiDollarSign></FiDollarSign></span>
+                    <p className="font-medium text-xl ">Price: {price}</p> 
+                </div>
+                <div className='flex justify-center gap-1 items-center '>
+                <span className='text-3xl'><BsBook></BsBook></span>
                 <p className="font-medium text-xl"> Credit: {credit_hour}hr</p>
+                </div>
                 
             </div>
             <button onClick={()=>selectedBtn(card)} className="bg-cyan-500 w-full my-4 py-2 text-2xl font-semibold text-white rounded-xl ">Select</button>
